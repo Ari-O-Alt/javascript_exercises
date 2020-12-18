f6af417e5f66d44b17c0daf8dbd5e91b22d2eee6
Silvia Bogdan <titifursec@outlook.com>
2020-12-18 15:42:11 +0100

    Position the button on the page; styled the letters

--- pages/magicLetters/magicLetters.js
+++ pages/magicLetters/magicLetters.js
@@ -1,5 +1,4 @@
 const appBox = document.getElementById('app');
-console.log(appBox);
 const button = document.getElementById('button');
 
 const convertString = (string) => {
@@ -7,19 +6,28 @@ const convertString = (string) => {
   return stringToArray;
 };
 
-const reverseArray = (theString) => {
+const printArrayToPage = (theString) => {
   const stringToArray = convertString(theString);
   console.log(stringToArray);
   stringToArray.forEach((element) => {
     const textBox = document.createElement('div');
-    textBox.classList.add('letter');
-    const text = document.createTextNode(element);
-    textBox.appendChild(text);
-    appBox.appendChild(textBox);
+
+    // if we have a letter, give the element a class
+    if (element !== ' ') {
+      textBox.classList.add('letter');
+    } else {
+      // else give it another one
+      textBox.classList.add('emptySpace');
+    }
+
+    const text = document.createTextNode(element); // create the text for the newly created div
+    textBox.appendChild(text); // append the text to the div
+    appBox.appendChild(textBox); // append the div to the app div
   });
 };
 
-button.addEventListener('click', () => reverseArray('Moroco'));
+window.onload(printArrayToPage('HELLO WORLD!'));
+// button.addEventListener('click', () => reverseArray('Hello world'));
 /* let rest = stringToArray.splice(0, 1);
     stringToArray.push(...rest);
   console.log(convertString("Alina")); */
