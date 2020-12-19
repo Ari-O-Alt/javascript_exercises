42280b2f71a45d58c3fa1fdc3ad903403737f5bd
Silvia Bogdan <titifursec@outlook.com>
2020-12-19 10:35:57 +0100

    Added more functions

--- pages/magicLetters/magicLetters.js
+++ pages/magicLetters/magicLetters.js
@@ -1,15 +1,18 @@
 const appBox = document.getElementById('app');
 const button = document.getElementById('button');
 
+// create function that converts string to array
 const convertString = (string) => {
   let stringToArray = string.split('');
   return stringToArray;
 };
 
-const printArrayToPage = (theString) => {
-  const stringToArray = convertString(theString);
-  console.log(stringToArray);
-  stringToArray.forEach((element) => {
+// cobvert the string to array
+const stringToArray = convertString('Hello world!');
+
+// create function that print the content of an array to the page
+const printArrayToPage = (string) => {
+  string.forEach((element) => {
     const textBox = document.createElement('div');
 
     // if we have a letter, give the element a class
@@ -27,20 +30,18 @@ const printArrayToPage = (theString) => {
 };
 
 // print the letters on the page when the page is loaded
-window.onload = function () {
-  printArrayToPage('HELLO WORLD!');
-};
+window.onload = printArrayToPage(stringToArray);
 
+// create function that makes a letter dissapear when clicked and it gets attached to the end of the array
 const moveLetter = (event) => {
-  const target = event.target;
-  console.log(target);
+  const targetLetter = event.target;
+  console.log(targetLetter);
+  targetLetter.classList.add('invisible');
+  stringToArray.push(targetLetter);
 };
 
+// select all letters/ buttons and add event listeners to them
 const allLetters = document.querySelectorAll('.letter');
-allLetters.forEach((element) => element.addEventListener('click', moveLetter, { once: true }));
-const allSpaces = document.querySelectorAll('emptySpace');
-allSpaces.forEach((element) => element.addEventListener('click', moveLetter, { once: true }));
-// button.addEventListener('click', () => reverseArray('Hello world'));
-/* let rest = stringToArray.splice(0, 1);
-    stringToArray.push(...rest);
-  console.log(convertString("Alina")); */
+allLetters.forEach((element) => element.addEventListener('click', moveLetter));
+const allSpaces = document.querySelectorAll('.emptySpace');
+allSpaces.forEach((element) => element.addEventListener('click', moveLetter));
